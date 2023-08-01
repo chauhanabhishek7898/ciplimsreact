407dfb107cf6b7dbea20b45ecb89412c89f0d089
abhishek chauhan <abhishekchauhan7898992278@gmail.com>
2023-08-01 18:28:43 +0530

    new changes

--- src/coreservices/environment.js
+++ src/coreservices/environment.js
@@ -1,8 +1,8 @@
-// const apiUrl = 'https://gallaimsapi.azurewebsites.net/api';
+const apiUrl = 'https://gallaimsapi.azurewebsites.net/api';
 // const signalrapiUrl = 'https://dromewebapi.azurewebsites.net/ChatHub';
-const apiUrlAddEdit = 'http://localhost:3000';
-// const apiUrlAddEdit = 'https://gallaims.azurewebsites.net';
-const apiUrl = 'http://localhost:5000/api';
+// const apiUrlAddEdit = 'http://localhost:3000';
+const apiUrlAddEdit = 'https://gallaims.azurewebsites.net';
+// const apiUrl = 'http://localhost:5000/api';
 const imageUrl = 'https://gallaimsapi.azurewebsites.net';
 //const imageUrl = 'http://localhost:5000'
 
